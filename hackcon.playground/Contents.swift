6fc79bf945dc8ed059d336e4db6710f220c3e05d
Abdulhakim Ajetunmobi <abdulrapidz@live.com>
2017-09-16 11:48:11 +0100

    Add files via upload

---
+++ hackcon.playground/Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Starting from [0]
+
+let name = "Abdulhakim Ajetunmobi"
+
+let twitter = "@abdulajet"
+
+var society = [String](repeating: "", count: 5)
+
+society[0] = "Team"
+
+society[1] = "Members"
+
+society[2] = "Support"
+
+society[3] = "Growing"
+
+society[4] = "Legacy"
+
+print(name)
+
+print(twitter)
+
+print(society)
